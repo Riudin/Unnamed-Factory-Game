cd23d16f94a5d013fb8864308d99a0aa2f9a0178
Chris <christoph@sengestack.de>
2026-01-16 17:18:15 +0100

    Improved Building
    Not done

--- Util/Components/building_handler.gd
+++ Util/Components/building_handler.gd
@@ -11,41 +11,90 @@ extends Node2D
 const TILE_SIZE: float = 16.0
 
 var current_building: PackedScene
-var is_building: bool = false
+var preview_active: bool = false
 var is_removing: bool = false
+
+var start_tile: Vector2
+var end_tile: Vector2
+
+var last_previewed_tiles: Array = []
 
 
 func _ready() -> void:
 	current_building = conveyor_belt
 
 func _process(_delta):
-	if is_building:
-		place_building()
+	if preview_active:
+		show_building_preview()
 	
 	if is_removing:
-		remove_building()
+		remove_building(tilemap_ground_layer.local_to_map(get_global_mouse_position()))
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			is_building = true
-			#print(tilemap_ground_layer.local_to_map(get_global_mouse_position()))
-		elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-			is_building = false
+			start_tile = tilemap_ground_layer.local_to_map(get_global_mouse_position())
+			preview_active = true
+		
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+			end_tile = tilemap_ground_layer.local_to_map(get_global_mouse_position())
+			
+			preview_active = false
+			
+			#var path := build_belt_path(start_tile, end_tile)
+			#for i in path.size() - 1:
+				#var dir = direction_from_to(path[i], path[i + 1])
+				#place_building(path[i], dir)
+			#place_building(path[-1], direction_from_to(path[-2], path[-1]))
 		
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			is_removing = true
 		elif event.button_index == MOUSE_BUTTON_RIGHT and not event.pressed:
 			is_removing = false
 
-func place_building():
+func build_belt_path(start: Vector2, end: Vector2) -> Array[Vector2]:
+	var path: Array[Vector2] = []
+	
+	var dx := end.x - start.x
+	var dy := end.y - start.y
+	
+	if abs(dx) >= abs(dy):
+		for x in range(start.x, end.x, sign(dx)):
+			path.append(Vector2(x, start.y))
+		for y in range(start.y, end.y, sign(dy)):
+			path.append(Vector2(end.x, y))
+	else:
+		for y in range(start.y, end.y, sign(dy)):
+			path.append(Vector2(start.x, y))
+		for x in range(start.x, end.x, sign(dx)):
+			path.append(Vector2(x, end.y))
+	
+	path.append(end)
+	return path
+
+func direction_from_to(a: Vector2, b: Vector2):
+	var direction := b - a
+	return direction
+
+func show_building_preview():
+	if last_previewed_tiles.size() > 0:
+		for i in last_previewed_tiles.size():
+			remove_building(last_previewed_tiles[i])
+		last_previewed_tiles.clear()
+	
+	end_tile = tilemap_ground_layer.local_to_map(get_global_mouse_position())
+	
+	var path := build_belt_path(start_tile, end_tile)
+	for i in path.size() - 1:
+		var dir = direction_from_to(path[i], path[i + 1])
+		place_building(path[i], dir, Color.AQUA)
+		last_previewed_tiles.append(path[i])
+	place_building(path[-1], Vector2.RIGHT, Color.AQUA)
+	last_previewed_tiles.append(path[-1])
+
+func place_building(tile: Vector2, to_direction: Vector2, color: Color = Color.WHITE):
 	if current_building == null:
 		return
-	
-	var mouse_position: Vector2 = get_global_mouse_position()
-	var tile: Vector2 = tilemap_ground_layer.local_to_map(
-		tilemap_ground_layer.to_local(mouse_position)
-		)
 	
 	if GridRegistry.is_occupied(tile):
 		return
@@ -57,15 +106,12 @@ func place_building():
 	var building = current_building.instantiate()
 	building.global_position = snapped_world_position
 	building.tile_coordinates = tile
+	building.to_direction = to_direction
+	building.modulate = color
 	building.register()
 	get_tree().current_scene.add_child(building)
 
-func remove_building():
-	var mouse_position: Vector2 = get_global_mouse_position()
-	var tile := tilemap_ground_layer.local_to_map(
-		tilemap_ground_layer.to_local(mouse_position)
-	)
-	
+func remove_building(tile: Vector2):
 	var building = GridRegistry.get_building(tile)
 	if building:
 		building.queue_free()
